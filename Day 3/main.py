252d8d4eb210243b4f3856d3a0d4dd0c9d42ae2c
Vortex <126605643+Vortexfnf@users.noreply.github.com>
2023-07-16 11:03:07 +0000

    Add the discord bot files

---
+++ Day 3/main.py
@@ -0,0 +1,26 @@
+import discord
+from bot_logic import gen_pass
+
+# The intents variable stores the bot's priviliges
+intents = discord.Intents.default()
+# Enabling the message-reading privelege
+intents.message_content = True
+# Creating a bot in the client variable and transferring it the priveleges
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$gen_pass'):
+        await message.channel.send("Your password " + gen_pass(15))
+
+client.run("MY TOKEN")
